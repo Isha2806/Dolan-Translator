476437cc19987545259192b3bf788bff4e253b8b
Isha <xishaapatel@gmail.com>
2021-09-24 09:48:34 +0530

    added selectors

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('hey isha ');
+var textInput = document.querySelector("#text-input");
+
+var outputDiv = document.querySelector("#output-div");
+
+var translateButton = document.querySelector("#trans-button");
+
+var clearButton = document.querySelector("#clear-button");
