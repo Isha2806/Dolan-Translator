3bbc9296e172e08650a105b47891be1ce29969b7
Isha <xishaapatel@gmail.com>
2021-09-24 09:50:58 +0530

    feat: added functions

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ var outputDiv = document.querySelector("#output-div");
 var translateButton = document.querySelector("#trans-button");
 
 var clearButton = document.querySelector("#clear-button");
+function clickHandler(event) {
+    if (textInput.value !== "") {
+        executeFetchCall(textInput.value)
+    }
+    else alert("Please enter text");
+}
+
+clearButton.addEventListener('click',clearClickHandler)
+translateButton.addEventListener('click', clickHandler);
